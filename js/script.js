66c7d6f69b7961fed3d11c16eeabaf49f877032f
Paul Johnston <tu98g3d6c@gmail.com>
2014-05-17 13:29:50 -0500

    Changed paragraphs to buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-    $("body").append("<p>I'm a new paragraph!</p>");
+    displayGrid(16);
 });
+
+function displayGrid (n) {
+	var size = 800;
+	var boxSize = (800 - n+1)/n;
+	var wrap = $("#grid").html("");
+	for (var j = 0; j < n; j++) {
+		for (var i = 0; i < n; i++) {
+			wrap.append( $("<div></div>").addClass("square").height(boxSize).width(boxSize) );
+		}
+		wrap.append($("<div></div>").css("clear", "both"));
+	}
+}
