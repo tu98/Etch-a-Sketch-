ddf453cf22ad3811f212a0fa670f739c53b64439
Paul Johnston <tu98g3d6c@gmail.com>
2014-05-19 22:00:34 -0500

    Finished puny function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,4 @@
-/* I followed the directions to this project pretty closely 
-except I don't ask the user for an input for the grid size
-IMHO this actually gives the user too much choice
-since I found it very annoying to have to type in a number
-each time I wanted to try out a new sketch game.
 
-Instead I decided to give the user three different buttons for grid 
-size choice. I wrote my displayGrid function to take any n
-to keep with the spirit of the challenge.
-
-This led to an interesting problem. I wanted the user to choose 
-a grid size and be able to select new sketch games without having
-to re-difine their prefered grid. Likewise I wanted the user 
-To be in a a sketch game, change the grid size, and not
-have to redefine their current game mode.
-
-This is why I save it in the two variables
-
-
-This way of doing things also gave the advantage of being able to run
-several game modes on the same grid which I chose not to allow
-by running the backToBlack() function before each game mode
-
-Another advantage is that I'm even able to run several game modes
-at once
-
-//check out callee to gt function name
-
-
-
-
-
-*/
 $(document).ready(function() {
     var currentHi;
     //currentGame ="vanilla";
@@ -64,17 +32,6 @@ function displayGrid () { //n used to be param
 		wrap.append($("<div></div>").css("clear", "both"));
 	}
 
-	//var currentGame = "vanilla";
-
-
-	//Eval is apparently bad practice
-	/*$('.gameRunning').each(function() {
-		//var x = this.id;
-		eval(($(this).id)+"()");
-	});*/
-	//var x = $('.controls.id')
-	//eval(currentGame+"()");
-	//eval(x+"()");
 
 	eval($('.gameRunning').attr('id')+'()');
 
@@ -93,33 +50,7 @@ function fruit () {
 	});
 }
 
-/*function tail() {
-	$(".square").hover(function() {
-		$(this).css("opacity", 1);
-	}, function () {
-		$(this).fadeTo('fast', 0);
-	});
-}
-*/
 
-/*function tail() {
-	$('.square').mouseover(function() {
-		$(this).css("opacity", 1);
-	}).mouseout(function() {
-		$(this).stop().animate({backgroundColor: 'black'} 1000);
-	});
-}*/
-
-
-
-
-//working one!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-/*function tail() {
-	$(".squares").hover(function() {
-	$(this).css("opacity", 1);
-	}, function () {
-	$(this).fadeTo('fast', 0);
-});*/
 function tail() {
 	$('.square').mouseenter(function() {
 		$(this).css('opacity', 0);
@@ -128,12 +59,12 @@ function tail() {
 }
 
 
+function puny() {
+	$('.square').mouseenter(function() {
+		$(this).css('opacity', $(this).css('opacity') - 0.1);
+});
+}
 
-
-
-
-//Own function since it will call this when choosing other 
-//sketch modes
 function backToBlack () {
 	displayGrid();
 }
